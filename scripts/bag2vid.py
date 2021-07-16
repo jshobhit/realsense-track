92c67550436fb24cb068ee4d7543e2acf833fcf1
shobhit <shobhit.jaipurkar@gmail.com>
2021-07-16 22:19:53 +0800

    Changes to be committed: 	modified:   scripts/bag2vid.py

--- scripts/bag2vid.py
+++ scripts/bag2vid.py
@@ -1,24 +1,25 @@
 # bag to videos
 
 from rosbag.bag import Bag
-import cv2
+import yaml
 import os
 import sys
 
-bag = Bag(sys.argv[1]) if os.path.exists(sys.argv[1]) else None
+bagfile = Bag(sys.argv[1]) if os.path.exists(sys.argv[1]) else None
+total_time = yaml.load(Bag(sys.argv[1])._get_yaml_info())['duration']
 
-ref_dict = {'/device_0/sensor_1/Color_0/image/data': 'color',
-        '/device_0/sensor_0/Infrared_0/image/data': 'infra',
-        '/device_0/sensor_0/Depth_0/image/data': 'depth',
-        '/device_0/sensor_0/Confidence_0/image/data': 'confidence'}
+color_msgs = bagfile.read_messages(topics = '/device_0/sensor_1/Color_0/image/data')
+depth_msgs = bagfile.read_messages(topics = '/device_0/sensor_0/Depth_0/image/data')
+infra_msgs = bagfile.read_messages(topics = '/device_0/sensor_0/Infrared_0/image/data')
 
-top = {v:k for k,v in ref_dict.iteritems()}
+color_fps = len([x for x in color_msgs]) // total_time
+depth_fps = len([x for x in depth_msgs]) // total_time
+infra_fps = len([x for x in infra_msgs]) // total_time
 
-if bag is not None:
+print('Color FPS: {0}\n\nDepth FPS: {1}\n\nInfra FPS: {2}'.format(color_fps, depth_fps, infra_fps))
 
-    color_msgs = bag.read_messages(topics = top['color'])
-    depth_msgs =  bag.read_messages(topics = top['depth'])
-    infra_msgs = bag.read_messages(topics = top['depth'])
-   
-    print(list(map(len, [color_msgs, depth_msgs, infra_msgs])))
+colorsavepath = raw_input('Enter the save path for the color feed')
+print('Generating command for saving video')
 
+cmd = 'rosrun image_view video_saver image:={0} --fps {1} --filename {2}'.format('/device_0/sensor_1/Color_0/image/data', color_fps, colorsavepath)
+print(cmd)
